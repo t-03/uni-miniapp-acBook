8061bbbcdb58e9dc2d491f9948be1d24bfc11d69
YuQi <692633615@qq.com>
2022-04-15 00:03:49 +0800

    修改部分内容

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -33,6 +33,10 @@ export default {
     }
     let uniIdToken = uni.getStorageSync("uni_id_token");
   },
+  mounted() {
+    let menuButtonInfo = uni.getMenuButtonBoundingClientRect();
+    console.log("----", menuButtonInfo);
+  },
   methods: {
     ...mapMutations(["login"]),
     guideToLogin() {
