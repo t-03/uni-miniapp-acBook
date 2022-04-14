0bbc1413cf18417a1464bf77c1b9de87acac1ac4
YuQi <692633615@qq.com>
2022-04-14 22:38:53 +0800

    删除登录弹窗

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -32,39 +32,6 @@ export default {
       return;
     }
     let uniIdToken = uni.getStorageSync("uni_id_token");
-    if (uniIdToken) {
-      this.login(uni.getStorageSync("username"));
-      uniCloud.callFunction({
-        name: "user-center",
-        data: {
-          action: "checkToken"
-        },
-        success: e => {
-          console.log("checkToken success", e);
-
-          if (e.result.code > 0) {
-            //token过期或token不合法，重新登录
-            if (this.forcedLogin) {
-              uni.reLaunch({
-                url: "../login/login"
-              });
-            } else {
-              uni.navigateTo({
-                url: "../login/login"
-              });
-            }
-          }
-        },
-        fail(e) {
-          uni.showModal({
-            content: JSON.stringify(e),
-            showCancel: false
-          });
-        }
-      });
-    } else {
-      this.guideToLogin();
-    }
   },
   methods: {
     ...mapMutations(["login"]),
